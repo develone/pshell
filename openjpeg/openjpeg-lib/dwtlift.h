f33f35a08fc1858f34727ec986666a455631ff56
Edward Vidal Jr <develone@sbcglobal.net>
2026-06-11 10:30:25 -0600

    openjpeg-lib/dwt.c           openjpeg-lib/dwt.h           openjpeg-lib/image.c           openjpeg-lib/j2k.c           openjpeg-lib/j2k.h           openjpeg-lib/jp2.c           openjpeg-lib/jp2.h           openjpeg-lib/mct.c           openjpeg-lib/mct.h           openjpeg-lib/mqc.c           openjpeg-lib/mqc.h           openjpeg-lib/mqc_inl.h           openjpeg-lib/openjpeg.c           openjpeg-lib/openjpeg.h           openjpeg-lib/opj_codec.h           openjpeg-lib/opj_includes.h           openjpeg-lib/opj_intmath.h           openjpeg-lib/pi.c           openjpeg-lib/t1.c           openjpeg-lib/t1.h           openjpeg-lib/t2.c           openjpeg-lib/t2.h           openjpeg-lib/tcd.c           openjpeg-lib/tcd.h           openjpeg-lib/thread.c           openjpeg-lib/dwtlift.c           openjpeg-lib/dwtlift.h

---
+++ openjpeg/openjpeg-lib/dwtlift.h
@@ -0,0 +1,16 @@
+
+double memory;
+ 
+int loop, decomp, encode;
+struct timeval currentTime,start,end;
+ 
+long mtime, seconds, useconds;
+
+double sqrt(double x);
+
+//void lift_config(struct CompressImage *s); 
+//void lift_config(int dec, int enc, int TCP_DISTORATIO, int FILTER,  int CR, int flg, int bp, long imgsz,long him,long wim, char *bufferptr);
+void decom_test(int x0, int y0, int x1, int y1,char *ff_in);
+ 
+
+typedef int int32;
